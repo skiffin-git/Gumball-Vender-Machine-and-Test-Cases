6d13f73a5532f8bfa4ff1b5d6a477e7387265435
Vladislav Semenyutin <semenyutin.v@gmail.com>
2023-02-03 14:02:25 -0800

    fixed documentation

--- untitled/src/main/java/org/example/GumballMachine.java
+++ untitled/src/main/java/org/example/GumballMachine.java
@@ -4,9 +4,12 @@ package org.example;
 import java.util.Scanner;
 
 /**
- * The `GumballMachine` class implements a simple gumball machine that dispenses red or yellow gumballs.
- * Users can insert coins of value 5, 10, or 25 cents and then dispense a gumball of a chosen color.
- * The user can also return their remaining coins if they wish to quit using the machine.
+ * The `GumballMachine` class implements a simple gumball machine
+ *      that dispenses red or yellow gumballs.
+ * Users can insert coins of value 5, 10, or 25 cents
+ *      and then dispense a gumball of a chosen color.
+ * The user can also return their remaining coins
+ *      if they wish to quit using the machine.
  */
 public class GumballMachine {
 
@@ -16,23 +19,26 @@ public class GumballMachine {
 
     /**
      * This method accepts an integer input that represents
-     * the coin inserted in the gumball machine.
+     *      the coin inserted in the gumball machine.
      * @param coin The value of the inserted coin in cents.
+     * @return a boolean indicating whether the inserted coin is valid or not.
      */
     public void insertCoin(int coin) {
         if (coin == 5 || coin == 10 || coin == 25) {
             balance += coin;
             System.out.println("New balance is: " + balance + " cents");
         } else {
-            System.out.println("Invalid input, returned " + coin + " on the push of the dispenses lever");
+            System.out.println("Invalid input, returned " + coin
+                    + " on the push of the dispenses lever");
         }
     }
 
     /**
      * This method dispenses a gumball of a specified color (red or yellow),
-     * provided the user has inserted enough coins to pay for the gumball.
+     *      provided the user has inserted enough coins to pay for the gumball.
      * @param input A string representing the color of the gumball
-     * to be dispensed (either "r" or "y").
+     *      to be dispensed (either "r" or "y").
+     * @return Returns `true` if gumball is dispensed successfully, `false` otherwise.
      */
     public boolean dispenseGumball(String input) {
         if (input.equalsIgnoreCase("r")) {
@@ -40,14 +46,16 @@ public class GumballMachine {
                 balance -= redGumballPrice;
                 System.out.println("Dispensing Red gumball, remaining coins: " + balance);
             } else {
-                System.out.println("Not enough coins inserted. Please insert " + (redGumballPrice - balance) + " cents more");
+                System.out.println("Not enough coins inserted. Please insert "
+                        + (redGumballPrice - balance) + " cents more");
             }
         } else if (input.equalsIgnoreCase("y")){
             if (balance >= yellowGumballPrice) {
                 balance -= yellowGumballPrice;
                 System.out.println("Dispensing Yellow gumball");
             } else {
-                System.out.println("Not enough coins inserted. Please insert " + (yellowGumballPrice - balance) + " cents more");
+                System.out.println("Not enough coins inserted. Please insert "
+                        + (yellowGumballPrice - balance) + " cents more");
             }
         } else {
             return false;
@@ -55,9 +63,12 @@ public class GumballMachine {
         return true;
     }
     /**
-     * This method returns the remaining coins in the user's balance if they are present.
-     * @param input A string representing the user's intention
-     * to return their remaining coins (either "g" or "G").
+     * This method returns the remaining coins in the user's balance if they are present,
+     *      and returns a boolean indicating if the coin return operation was successful.
+     * @param input A string representing the user's intention to return their
+     *              remaining coins (either "g" or "G").
+     * @return A boolean indicating if the coin return operation
+     *      was successful (true) or not (false).
      */
     public boolean returnCoins(String input) {
         if (input.equalsIgnoreCase("g")) {
